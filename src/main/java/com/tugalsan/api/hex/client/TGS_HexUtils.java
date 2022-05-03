d748ce9f0380457eadb7ea0e0c65a7a089bb113b
tugalsan karabacak <tugalsan@gmail.com>
2022-05-03 10:56:45 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/hex/client/TGS_HexUtils.java
@@ -0,0 +1,48 @@
+package com.tugalsan.api.hex.client;
+
+import java.util.Locale;
+import java.util.stream.IntStream;
+
+public class TGS_HexUtils {
+
+    public static String toHex(int value_0_255) {
+        var sb = new StringBuilder();
+        sb.append(dictionary().charAt((value_0_255 - value_0_255 % 16) / 16));
+        sb.append(dictionary().charAt(value_0_255 % 16));
+        return sb.toString();
+    }
+
+    private static String dictionary() {
+        return "0123456789ABCDEF";
+    }
+
+    public static String toHex(int[] values_0_255) {
+        var sb = new StringBuilder(values_0_255.length * 2);
+        IntStream.range(0, values_0_255.length).forEachOrdered(i -> sb.append(toHex(values_0_255[i])));
+        return sb.toString().toUpperCase(Locale.ROOT);
+    }
+
+    public static int toInt(char hexChar1, char hexChar0) {
+        return Integer.parseInt(new StringBuilder().append(hexChar1).append(hexChar0).toString(), 16);
+    }
+
+    public static int[] toInt(CharSequence hex_withPairChars) {
+        var b = new int[hex_withPairChars.length() / 2];
+        IntStream.range(0, b.length).parallel().forEach(i -> {
+            var index = i * 2;
+            b[i] = TGS_HexUtils.toInt(hex_withPairChars.charAt(index), hex_withPairChars.charAt(index + 1));
+        });
+        return b;
+    }
+
+    public static String toHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        char[] dictionary = dictionary().toCharArray();
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = dictionary[v >>> 4];
+            hexChars[j * 2 + 1] = dictionary[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.tugalsan.api.hex {
+    exports com.tugalsan.api.hex.client;
+}
